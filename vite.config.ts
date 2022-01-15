c86e462edffc8b4d354348a5471302c0d5efa17c
weiquanju <anbine@qq.com>
2022-01-15 16:47:16 +0800

    feat: surport jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname
